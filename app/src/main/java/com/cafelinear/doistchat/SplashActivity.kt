40610f16f5b9c3967e91cb247ee891a80a50899d
Douglas Drumond <douglas@cafelinear.com>
2018-08-12 13:14:45 -0300

    Initial commit

---
+++ app/src/main/java/com/cafelinear/doistchat/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.cafelinear.doistchat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
